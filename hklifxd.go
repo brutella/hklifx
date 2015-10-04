5f9e28c6ffda5dcf273963e4d093048bee534ab7
Pieter Maene <pieter@maene.eu>
2015-10-05 01:51:34 +0200

    The Local LAN API is now used through pdf/golifx

--- hklifxd.go
+++ hklifxd.go
@@ -2,174 +2,205 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
+	"math"
 	"os"
+	"time"
 
 	"github.com/brutella/hc/hap"
 	"github.com/brutella/hc/model"
 	"github.com/brutella/hc/model/accessory"
-	"github.com/wolfeidau/lifx"
-	"math"
-	"time"
+
+	"github.com/pdf/golifx"
+	"github.com/pdf/golifx/common"
+	"github.com/pdf/golifx/protocol"
 )
 
 const (
 	// from https://github.com/LIFX/LIFXKit/blob/master/LIFXKit/Classes-Common/LFXHSBKColor.h
 	HSBKKelvinDefault = uint16(3500)
-	HSBKKelvinMin     = uint16(2500)
-	HSBKKelvinMax     = uint16(9000)
+	HSBKKelvinMin = uint16(2500)
+	HSBKKelvinMax = uint16(9000)
 )
 
-func ConnectLIFX() {
-	client = lifx.NewClient()
-	err := client.StartDiscovery()
+type HKLight struct {
+	accessory *accessory.Accessory
+	sub *common.Subscription
+	transport hap.Transport
 
+	light model.LightBulb
+}
+
+var (
+	lights map[uint64] *HKLight
+	pin string
+
+	client *golifx.Client
+)
+
+func Connect() {
+	client, err := golifx.NewClient(&protocol.V2{Reliable: true})
 	if err != nil {
-		log.Fatalf("Could not find bulb %s", err)
+		log.Fatalf("[ERR] Failed to initiliaze the client: %s", err)
 	}
 
-	sub := client.Subscribe()
+	sub, _ := client.NewSubscription()
 	for {
-		event := <-sub.Events
-		switch event := event.(type) {
-		case *lifx.Gateway:
-		case *lifx.Bulb:
-			updateBulb(event)
-			event.SetStateHandler(func(newState *lifx.BulbState) {
-				log.Println("Updated", newState)
-				updateBulb(event)
-			})
-		default:
-			log.Printf("Event %v", event)
+		event := <-sub.Events()
+		switch event.(type) {
+			case common.EventNewLocation:
+				log.Printf("[INFO] Discovered Location %s", event.(common.EventNewLocation).Location.GetLabel())
+			case common.EventNewGroup:
+				log.Printf("[INFO] Discovered Group %s", event.(common.EventNewGroup).Group.GetLabel())
+			case common.EventNewDevice:
+				label, _ := event.(common.EventNewDevice).Device.GetLabel()
+				log.Printf("[INFO] Discovered Device %s", label)
+
+				go NewDevice(event.(common.EventNewDevice).Device)
+
+			case common.EventExpiredLocation:
+				log.Printf("[INFO] Expired Location %s", event.(common.EventExpiredLocation).Location.GetLabel())
+			case common.EventExpiredGroup:
+				log.Printf("[INFO] Expired Group %s", event.(common.EventExpiredGroup).Group.GetLabel())
+			case common.EventExpiredDevice:
+				label, _ := event.(common.EventExpiredDevice).Device.GetLabel()
+				log.Printf("[INFO] Expired Device %s", label)
+
+				ExpireDevice(event.(common.EventNewDevice).Device)
+
+			default:
+				log.Printf("[INFO] Unknown Client Event: %T", event)
 		}
 	}
 }
 
-func updateBulb(bulb *lifx.Bulb) {
-	on := true
-	if bulb.GetPower() == 0 {
-		on = false
-	}
+func NewDevice(device common.Device) {
+	if light, ok := device.(common.Light); ok {
+		hkLight := GetHKLight(light)
 
-	state := bulb.GetState()
+		hkLight.sub, _ = light.NewSubscription()
+		for {
+			event := <-hkLight.sub.Events()
+			switch event.(type) {
+				case common.EventUpdateLabel:
+					log.Printf("[INFO] Updated Label for %s to %s", hkLight.accessory.Name(), event.(common.EventUpdateLabel).Label)
+					// TODO Add support for label changes to HomeControl
+					log.Printf("[INFO] Unsupported by HomeControl")
+				case common.EventUpdatePower:
+					log.Printf("[INFO] Updated Power for %s", hkLight.accessory.Name())
+					hkLight.light.SetOn(event.(common.EventUpdatePower).Power)
+				case common.EventUpdateColor:
+					log.Printf("[INFO] Updated Color for %s", hkLight.accessory.Name())
 
-	name := bulb.GetLabel()
-	if light, found := lights[name]; found == true && state.Visible == false {
-		log.Printf("Remove light", light)
-		removeLight(light, name)
-		return
-	}
+					// TODO Define max variables in HomeControl
+					color := event.(common.EventUpdateColor).Color
 
-	light_bulb := lightForBulb(bulb).bulb
+					hue := float64(color.Hue) / float64(math.MaxUint16) * 360
+					saturation := float64(color.Saturation) / float64(math.MaxUint16) * 100
+					brightness := float64(color.Brightness) / float64(math.MaxUint16) * 100
 
-	light_bulb.SetOn(on)
+					hkLight.light.SetBrightness(int(brightness))
+					hkLight.light.SetSaturation(saturation)
+					hkLight.light.SetHue(hue)
 
-	brightness := float64(state.Brightness) / float64(math.MaxUint16) * 100
-	saturation := float64(state.Saturation) / float64(math.MaxUint16) * 100
-	hue := float64(state.Hue) / float64(math.MaxUint16) * 360
-
-	light_bulb.SetBrightness(int(brightness))
-	light_bulb.SetSaturation(saturation)
-	light_bulb.SetHue(hue)
-
-	log.Println("LIFX is now", on)
-	log.Println("Brightness", brightness)
-	log.Println("Saturation", saturation)
-	log.Println("Hue", hue)
-}
-
-func toggleBulb(bulb *lifx.Bulb) {
-	if bulb.GetPower() == 0 {
-		client.LightOn(bulb)
+				default:
+					log.Printf("[INFO] Unknown Device Event: %T", event)
+			}
+		}
 	} else {
-		client.LightOff(bulb)
+		log.Println("[INFO] Unsupported Device")
 	}
 }
-func removeLight(light *lifxLight, name string) {
-	light.transport.Stop()
-	delete(lights, name)
+
+func ExpireDevice(device common.Device) {
+	if light, ok := device.(common.Light); ok {
+		hkLight, _ := lights[light.ID()]
+		light.CloseSubscription(hkLight.sub)
+		hkLight.transport.Stop()
+
+		delete(lights, light.ID())
+	} else {
+		log.Println("[INFO] Unsupported Device")
+	}
 }
 
-func lightForBulb(bulb *lifx.Bulb) *lifxLight {
-	label := bulb.GetLabel()
-	light, found := lights[label]
-	if found == true {
-		fmt.Println("Found")
-		return light
+func GetHKLight(light common.Light) *HKLight {
+	hkLight, found := lights[light.ID()]
+	if found {
+		return hkLight
 	}
 
-	fmt.Println("Create new switch for blub")
+	label, _ := light.GetLabel()
+	log.Printf("[INFO] Creating New HKLight for %s", label)
 
 	info := model.Info{
 		Name:         label,
 		Manufacturer: "LIFX",
 	}
 
-	light_bulb := accessory.NewLightBulb(info)
-	light_bulb.OnIdentify(func() {
+	lightBulb := accessory.NewLightBulb(info)
+
+	lightBulb.OnIdentify(func() {
 		timeout := 1 * time.Second
-		toggleBulb(bulb)
-		time.Sleep(timeout)
-		toggleBulb(bulb)
-		time.Sleep(timeout)
-		toggleBulb(bulb)
-		time.Sleep(timeout)
-		toggleBulb(bulb)
+
+		for i := 0; i < 4; i++ {
+			ToggleLight(light)
+			time.Sleep(timeout)
+    	}
 	})
 
-	light_bulb.OnStateChanged(func(on bool) {
-		if on == true {
-			client.LightOn(bulb)
-			log.Println("Switch is on")
-		} else {
-			client.LightOff(bulb)
-			log.Println("Switch is off")
-		}
+	lightBulb.OnStateChanged(func(power bool) {
+		light.SetPower(power)
+		log.Printf("Changed State for %s", label)
 	})
 
-	updateColors := func(client *lifx.Client, bulb *lifx.Bulb) {
-		// TODO define max variables in Gohap
-
+	updateColor := func(light common.Light) {
 		// HAP: [0...360]
 		// LIFX: [0...MAX_UINT16]
-		hue := light_bulb.GetHue()
+		hue := lightBulb.GetHue()
 
 		// HAP: [0...100]
 		// LIFX: [0...MAX_UINT16]
-		saturation := light_bulb.GetSaturation()
+		saturation := lightBulb.GetSaturation()
+
 		// HAP: [0...100]
 		// LIFX: [0...MAX_UINT16]
-		brightness := light_bulb.GetBrightness()
-		// [2500..9000]
+		brightness := lightBulb.GetBrightness()
+
+		// [HSBKKelvinMin..HSBKKelvinMax]
 		kelvin := HSBKKelvinDefault
 
-		lifx_brightness := math.MaxUint16 * float64(brightness) / 100
-		lifx_saturation := math.MaxUint16 * float64(saturation) / 100
-		lifx_hue := math.MaxUint16 * float64(hue) / 360
+		// TODO Define max variables in HomeControl
+		lifxHue := math.MaxUint16 * float64(hue) / 360
+		lifxSaturation := math.MaxUint16 * float64(saturation) / 100
+		lifxBrightness := math.MaxUint16 * float64(brightness) / 100
 
-		log.Println("Brightness", lifx_brightness)
-		log.Println("Hue", lifx_saturation)
-		log.Println("Saturation", lifx_hue)
-		client.LightColour(bulb, uint16(lifx_hue), uint16(lifx_saturation), uint16(lifx_brightness), uint16(kelvin), 0x0500)
+		color := common.Color{
+			uint16(lifxHue),
+			uint16(lifxSaturation),
+			uint16(lifxBrightness),
+			kelvin,
+		}
+
+		light.SetColor(color, 500 * time.Millisecond)
 	}
 
-	light_bulb.OnBrightnessChanged(func(value int) {
-		log.Println("Brightness", value)
-		updateColors(client, bulb)
+	lightBulb.OnHueChanged(func(value float64) {
+		log.Printf("[INFO] Changed Hue for %s to %d", label, value)
+		updateColor(light)
 	})
 
-	light_bulb.OnSaturationChanged(func(value float64) {
-		log.Println("Saturation", value)
-		updateColors(client, bulb)
+	lightBulb.OnSaturationChanged(func(value float64) {
+		log.Printf("[INFO] Changed Saturation for %s to %d", label, value)
+		updateColor(light)
 	})
 
-	light_bulb.OnHueChanged(func(value float64) {
-		log.Println("Hue", value)
-		updateColors(client, bulb)
+	lightBulb.OnBrightnessChanged(func(value int) {
+		log.Printf("[INFO] Changed Brightness for %s to %d", label, value)
+		updateColor(light)
 	})
 
-	transport, err := hap.NewIPTransport(pin, light_bulb.Accessory)
+	transport, err := hap.NewIPTransport(pin, lightBulb.Accessory)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -178,39 +209,30 @@ func lightForBulb(bulb *lifx.Bulb) *lifxLight {
 		transport.Start()
 	}()
 
-	light = &lifxLight{transport, light_bulb, light_bulb.Accessory}
-	lights[label] = light
-
-	return light
+	hkLight = &HKLight{lightBulb.Accessory, nil, transport, lightBulb}
+	return hkLight
 }
 
-type lifxLight struct {
-	transport hap.Transport
-	bulb      model.LightBulb
-	accessory *accessory.Accessory
+func ToggleLight(light common.Light) {
+	power, _ := light.GetPower()
+	light.SetPower(!power)
 }
-
-var lights map[string]*lifxLight
-var client *lifx.Client
-var pin string
 
 func main() {
-	var (
-		pinArg = flag.String("pin", "", "Accessory pin used for pairing")
-	)
+	lights = map[uint64]*HKLight{}
 
+	pinArg := flag.String("pin", "", "PIN used to pair the LIFX bulbs with HomeKit")
 	flag.Parse()
 	pin = *pinArg
 
-	lights = map[string]*lifxLight{}
-
 	hap.OnTermination(func() {
-		for _, l := range lights {
-			l.transport.Stop()
+		for _, light := range lights {
+			light.transport.Stop()
 		}
+
 		time.Sleep(100 * time.Millisecond)
 		os.Exit(1)
 	})
 
-	ConnectLIFX()
+	Connect()
 }
