4d73e6b9f49e136922ce516cb4069b7b86ca5333
Matthias Hochgatterer <matthias.hochgatterer@gmail.com>
2016-10-06 10:40:50 +0200

    Squashed 'vendor/github.com/brutella/hc/' content from commit e2d915f
    git-subtree-dir: vendor/github.com/brutella/hc
    git-subtree-split: e2d915f38ed0961bb25d048f2b7bd84e168cb9d2

---
+++ accessory/constant.go
@@ -0,0 +1,23 @@
+package accessory
+
+type AccessoryType int
+
+const (
+	TypeUnknown            AccessoryType = 0
+	TypeOther              AccessoryType = 1
+	TypeBridge             AccessoryType = 2
+	TypeFan                AccessoryType = 3
+	TypeGarageDoorOpener   AccessoryType = 4
+	TypeLightbulb          AccessoryType = 5
+	TypeDoorLock           AccessoryType = 6
+	TypeOutlet             AccessoryType = 7
+	TypeSwitch             AccessoryType = 8
+	TypeThermostat         AccessoryType = 9
+	TypeSensor             AccessoryType = 10
+	TypeAlarmSystem        AccessoryType = 11
+	TypeDoor               AccessoryType = 12
+	TypeWindow             AccessoryType = 13
+	TypeWindowCovering     AccessoryType = 14
+	TypeProgrammableSwitch AccessoryType = 15
+	TypeIPCamera           AccessoryType = 17
+)
